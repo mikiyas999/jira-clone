91ab08d37f5a054e83650f8bd42fcf01e3b86eeb
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-01-12 06:51:57 +0000

    redirect non sign-in user to homepage

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,11 @@
 import { SignInCard } from "@/features/auth/components/sign-card";
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const SignIn = () => {
+const SignIn = async () => {
+  const user = await getCurrent();
+  if (user) redirect("/");
   return (
     <div className="h-full ">
       <SignInCard />
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,11 @@
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const SignUP = () => {
+const SignUP = async () => {
+  const user = await getCurrent();
+  if (user) redirect("/");
   return (
     <div>
       <SignUpCard />
