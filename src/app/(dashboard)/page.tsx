e838b40d3fd973c4825bdd295a51685f24ed3846
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-01-15 18:14:40 +0000

    implement dashboard page

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+import { getCurrent } from "@/features/auth/queries";
+
+export default async function Home() {
+  const current = await getCurrent();
+  if (!current) redirect("/sign-in");
+
+  return (
+    <div>
+      <h1>homepage</h1>
+    </div>
+  );
+}
