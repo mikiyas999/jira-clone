7579b1b6af19c7be18d333555a65ed19012b3b00
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-02-25 14:43:57 +0000

    implement calendar and kanban board

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -15,6 +15,7 @@ const routes = app
   .route("members", members)
   .route("/projects", projects)
   .route("/tasks", tasks);
+
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
